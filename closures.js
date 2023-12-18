00abb32a4b77f3e09c7241616bd5882728213a5f
Mike Grilli <grrillos80gmail.com>
2023-12-18 10:01:58 -0800

    updating assginment

--- closures.js
+++ closures.js
@@ -10,9 +10,11 @@ const createCounter = () => {
   };
 };
 const counter = createCounter();
+
+
 counter.increaseCount();
 console.log(counter.getCount());
-// 1
+
 
 // Exercise:
 // This function should create an empty array of messages, and should then
@@ -20,46 +22,46 @@ console.log(counter.getCount());
 // - addMessage method that adds a message to the array
 // - getMessage(index) method that returns the message at index index
 
-const createMessageHolder = () => {};
+// const createMessageHolder = () => {};
 
-// Test
-const messageHolder = createMessageHolder();
-messageHolder.addMessage('Hello!');
-messageHolder.addMessage('Goodbye!');
-console.log(messageHolder.getMessage(0));
-// "Hello!""
+// // Test
+// const messageHolder = createMessageHolder();
+// messageHolder.addMessage('Hello!');
+// messageHolder.addMessage('Goodbye!');
+// console.log(messageHolder.getMessage(0));
+// // "Hello!""
 
-// Example: non-currying
-const addNumbers = function(num1, num2) {
-  return num1 + num2;
-};
-console.log(addNumbers(5, 3));
-// 8
+// // Example: non-currying
+// const addNumbers = function(num1, num2) {
+//   return num1 + num2;
+// };
+// console.log(addNumbers(5, 3));
+// // 8
 
-// Example: currying
-const addToNumber = function(num1) {
-  const addToFirst = function(num2) {
-    return num1 + num2;
-  };
-  return addToFirst;
-};
-const addThree = addToNumber(3);
-console.log(addThree(9));
-// 12
+// // Example: currying
+// const addToNumber = function(num1) {
+//   const addToFirst = function(num2) {
+//     return num1 + num2;
+//   };
+//   return addToFirst;
+// };
+// const addThree = addToNumber(3);
+// console.log(addThree(9));
+// // 12
 
-console.log(addThree(41));
-// 44
+// console.log(addThree(41));
+// // 44
 
 // Create a function createGreeting
 // - This should accept a single argument: greeting (i.e. "Hello")
 // This will return a function a function greet
 // - This accepts a single argument, name (i.e. "Matt")
 // - This function should return the greeting combined with the name, (i.e. "Hello Matt")
-const createGreeting = function(greeting) {};
+// const createGreeting = function(greeting) {};
 
-// Test
-const welcomeGreet = createGreeting('Welcome');
-console.log(welcomeGreet('Alice'));
+// // Test
+// const welcomeGreet = createGreeting('Welcome');
+// console.log(welcomeGreet('Alice'));
 
-const helloGreet = createGreeting('Hello');
-console.log(helloGreet('Winnie'));
+// const helloGreet = createGreeting('Hello');
+// console.log(helloGreet('Winnie'));
--- higher-order.js
+++ higher-order.js
@@ -1,22 +1,16 @@
 // This function should execute the callback function the number of times specified.
 // When the function is being executed, the repetition number (i.e. 1 for the first call)
 // should be passed to the callback.
-const repeatFn = (times, callback) => {};
 
-// Test repeatFn
-const addButton = num => {
-  const button = document.createElement('button');
-  button.innerText = `Button ${num}`;
-  document.querySelector('body').appendChild(button);
-};
-repeatFn(6, addButton);
+const repeatFunction = (num, callback) => {
 
-const toThePower = (num, pow) => {
-  let product = 1;
-  repeatFn(pow, () => {
-    product += product * num;
-  });
-  return product;
-};
+    for (let i = 0; i < num; i++) {
+      callback(i)
+    }
+} 
 
-console.log(toThePower(3, 3));
+const doubleNumber = (num) => {
+  console.log(num * 2)
+}
+
+repeatFunction(5, doubleNumber)
